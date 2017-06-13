251923a6c59c132f30007e361a52d5f38b2a98ab
kost-g <kost-g@yandex.ru>
2017-06-13 11:29:39 +0300

    13/06/17

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $pdo = new PDO("mysql:host=localhost;dbname=absoluteTest", "root", "");
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="style/style.css" rel="stylesheet">
 
+
 </head>
 <body>
 <header>
